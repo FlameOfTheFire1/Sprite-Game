00c311c7a4981f2ddb82e1921293d7580fd9c50b
FlameOfTheFire1 <124635735+FlameOfTheFire1@users.noreply.github.com>
2023-04-06 22:10:39 -0700

    lots of stuff

---
+++ imageLoops.js
@@ -0,0 +1,98 @@
+var imageLoops = { images: [
+    {"East" :{
+        "0":{
+                "Img": "images/IMG_9269.JPG",
+                 "start":true
+                 },
+        "1":{
+                "Img": "images/IMG_9271.JPG",
+                "test":"You are facing south, you can go to position 1,0"
+                },
+        "2":{
+                "Img": "images/IMG_9270.JPG",
+                "test":"You are now facing East."
+                },
+        "3":{
+                "Img": "images/IMG_9272.JPG",
+                "end":true
+                }
+        }},
+
+
+    {"West":{
+        "0":{
+            "Img": "images/IMG_9273.JPG",
+            "start":true
+             },
+        "1":{
+            "Img": "images/IMG_9275.JPG",
+            "test":"You are facing south, you can go to position 1,0"
+        },
+        "2":{
+            "Img": "images/IMG_9274.JPG",
+            "test":"You are now facing East."
+        },
+        "3":{
+            "Img": "images/IMG_9276.JPG",
+            "end":true
+        }
+    }},
+   
+    {"North":{
+        "0":{
+            "Img": "images/IMG_9277.JPG",
+            "start":true
+             },
+        "1":{
+            "Img": "images/IMG_9279.JPG",
+            "test":"You are facing south, you can go to position 1,0"
+        },
+        "2":{
+            "Img": "images/IMG_9278.JPG",
+            "test":"You are now facing East."
+        },
+        "3":{
+            "Img": "images/IMG_9280.JPG",
+            "end":true
+        }
+    }},
+
+    {"South":{
+        "0":{
+            "Img": "images/IMG_9284.JPG",
+            "start":true
+             },
+        "1":{
+            "Img": "images/IMG_9282.JPG",
+            "test":"You are facing south, you can go to position 1,0"
+        },
+        "2":{
+            "Img": "images/IMG_9281.JPG",
+            "test":"You are now facing East."
+        },
+        "3":{
+            "Img": "images/IMG_9283.JPG",
+            "end":true
+        }
+    }},
+
+    {"Idle":{
+        "0":{
+            "Img": "images/IMG_9288.JPG",  
+            "start":true
+             },
+        "1":{
+            "Img": "images/IMG_9286.JPG",
+            "test":"You are facing south, you can go to position 1,0"
+        },
+        "2":{
+            "Img": "images/IMG_9285.JPG",
+            "test":"You are now facing East."
+        },
+        "3":{
+            "Img": "images/IMG_9287.JPG",
+            "end":true
+        }
+    }}
+]
+}
