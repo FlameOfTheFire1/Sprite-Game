c8300d183b980aab7ee395190c07f9fc236f848c
FlameOfTheFire1 <124635735+FlameOfTheFire1@users.noreply.github.com>
2023-04-10 15:31:26 -0700

    latest updates

--- imageLoops.js
+++ imageLoops.js
@@ -1,19 +1,19 @@
 var imageLoops = { images: [
     {"East" :{
         "0":{
-                "Img": "images/IMG_9269.JPG",
+                "Img": "images/walk_E/0.png",
                  "start":true
                  },
         "1":{
-                "Img": "images/IMG_9271.JPG",
+                "Img": "images/walk_E/1.png",
                 "test":"You are facing south, you can go to position 1,0"
                 },
         "2":{
-                "Img": "images/IMG_9270.JPG",
+                "Img": "images/walk_E/2.png",
                 "test":"You are now facing East."
                 },
         "3":{
-                "Img": "images/IMG_9272.JPG",
+                "Img": "images/walk_E/3.png",
                 "end":true
                 }
         }},
@@ -21,76 +21,76 @@ var imageLoops = { images: [
 
     {"West":{
         "0":{
-            "Img": "images/IMG_9273.JPG",
+            "Img": "images/walk_W/0.png",
             "start":true
              },
         "1":{
-            "Img": "images/IMG_9275.JPG",
+            "Img": "images/walk_W/1.png",
             "test":"You are facing south, you can go to position 1,0"
         },
         "2":{
-            "Img": "images/IMG_9274.JPG",
+            "Img": "images/walk_W/2.png",
             "test":"You are now facing East."
         },
         "3":{
-            "Img": "images/IMG_9276.JPG",
+            "Img": "images/walk_W/3.png",
             "end":true
         }
     }},
    
     {"North":{
         "0":{
-            "Img": "images/IMG_9277.JPG",
+            "Img": "images/walk_N/0.png",
             "start":true
              },
         "1":{
-            "Img": "images/IMG_9279.JPG",
+            "Img": "images/walk_N/1.png",
             "test":"You are facing south, you can go to position 1,0"
         },
         "2":{
-            "Img": "images/IMG_9278.JPG",
+            "Img": "images/walk_N/2.png",
             "test":"You are now facing East."
         },
         "3":{
-            "Img": "images/IMG_9280.JPG",
+            "Img": "images/walk_N/3.png",
             "end":true
         }
     }},
 
     {"South":{
         "0":{
-            "Img": "images/IMG_9284.JPG",
+            "Img": "images/walk_S/0.png",
             "start":true
              },
         "1":{
-            "Img": "images/IMG_9282.JPG",
+            "Img": "images/walk_S/1.png",
             "test":"You are facing south, you can go to position 1,0"
         },
         "2":{
-            "Img": "images/IMG_9281.JPG",
+            "Img": "images/walk_S/2.png",
             "test":"You are now facing East."
         },
         "3":{
-            "Img": "images/IMG_9283.JPG",
+            "Img": "images/walk_S/3.png",
             "end":true
         }
     }},
 
     {"Idle":{
         "0":{
-            "Img": "images/IMG_9288.JPG",  
+            "Img": "images/idle/0.png",  
             "start":true
              },
         "1":{
-            "Img": "images/IMG_9286.JPG",
+            "Img": "images/idle/1.png",
             "test":"You are facing south, you can go to position 1,0"
         },
         "2":{
-            "Img": "images/IMG_9285.JPG",
+            "Img": "images/idle/2.png",
             "test":"You are now facing East."
         },
         "3":{
-            "Img": "images/IMG_9287.JPG",
+            "Img": "images/idle/3.png",
             "end":true
         }
     }}
